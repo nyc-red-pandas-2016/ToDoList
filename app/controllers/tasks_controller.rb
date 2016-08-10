1a7ddcc87114dfd9a8a6344182da0bef23653031
Wolfgang Criollo <wolfplisken@gmail.com>
2016-08-10 00:20:07 -0400

    add session for creating a new task and appending to the list add the avility to delete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,9 @@
 get '/list/:id/tasks/new' do
   @list = List.find(params[:id])
-  session[:list_id]=@list.id
+  session[:list_id] = @list.id
   erb :"tasks/new"
 end
-# 
+#
 # get '/task/:id' do
 #   # session[:list_id].clear
 #
@@ -16,7 +16,16 @@ post '/tasks' do
     if @task.save
        redirect "/list/#{current_list.id}"
      else
+       @list = current_list
        @errors = @task.errors.full_messages
-       erb :"/list/#{current_list.id}/tasks/new"
+     erb :"/tasks/new"
     end
+end
+
+
+delete '/tasks/:id' do
+     task = Task.find(params[:id])
+      list_id = task.list_id
+      task.destroy
+    redirect "/list/#{list_id}"
 end
