b40ff4b830022271177b3665d0691c815e8f86fb
Myra Orgain <myraorgain@gmail.com>
2016-08-14 14:23:01 -0400

    Add user routes, flesh out index, registration, login pages with logout function

---
+++ app/controllers/user.rb
@@ -0,0 +1,39 @@
+get '/users/new' do
+	erb :'/users/new'
+end
+
+post '/users' do
+	user = User.new(params[:user])
+	if user.save
+		session[:user_id] = user.id
+    	redirect "/users/#{user.id}"
+  	else
+    	@errors = user.errors.full_messages
+    	erb :'/users/new'
+  	end
+end
+
+get '/users/login' do
+  erb :'/users/login'
+end
+
+post '/users/login' do
+  user = User.find_by(username: params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/users/#{user.id}"
+  else
+    @error = "Please enter a valid e-mail/password combination."
+    erb :'/users/login'
+  end
+end
+
+get '/users/logout' do
+  session.clear
+  redirect '/'
+end
+
+get '/users/:id' do
+  @user = User.find(params[:id])
+  erb :'/users/show'
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,40 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+	// Sticky Header
+	$(window).scroll(function() {
+
+	    if ($(window).scrollTop() > 100) {
+	        $('.main_h').addClass('sticky');
+	    } else {
+	        $('.main_h').removeClass('sticky');
+	    }
+	});
+
+	// Mobile Navigation
+	$('.mobile-toggle').click(function() {
+	    if ($('.main_h').hasClass('open-nav')) {
+	        $('.main_h').removeClass('open-nav');
+	    } else {
+	        $('.main_h').addClass('open-nav');
+	    }
+	});
+
+	$('.main_h li a').click(function() {
+	    if ($('.main_h').hasClass('open-nav')) {
+	        $('.navigation').removeClass('open-nav');
+	        $('.main_h').removeClass('open-nav');
+	    }
+	});
+
+	// navigation scroll
+	$('nav a').click(function(event) {
+	    var id = $(this).attr("href");
+	    var offset = 70;
+	    var target = $(id).offset().top - offset;
+	    $('html, body').animate({
+	        scrollTop: target
+	    }, 500);
+	    event.preventDefault();
+	});
+
 });
