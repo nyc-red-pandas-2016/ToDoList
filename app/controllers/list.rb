36689580ae8df560bc1f4b8f49881b64e8420d7c
Richie Ito <email": null>
2016-08-16 18:40:31 -0400

    finish non ajax crud

--- app/controllers/list.rb
+++ app/controllers/list.rb
@@ -1,8 +1,8 @@
-get '/lists/create' do
+get '/lists/new' do
   erb :'/lists/create'
 end
 
-post '/lists/create' do
+post '/lists' do
   list= List.new(params[:list])
 
   if list.save
@@ -15,7 +15,26 @@ end
 
 get '/lists/:id' do
   @list= List.find(params[:id])
-  @tasks= Task.where(list_id: @list.id)
+  @tasks= @list.tasks
   erb :'/lists/show'
 end
 
+get '/lists/:id/edit' do
+  @list= List.find(params[:id])
+  erb :'lists/edit'
+end
+
+put '/lists/:id' do
+  @list = List.find(params[:id])
+  @list.assign_attributes(params[:list])
+  if @list.save
+    redirect "/lists/#{@list.id}"
+  else
+    erb :'lists/edit'
+  end
+end
+
+delete '/lists/:id' do
+  List.find(params[:id]).destroy
+  redirect "/users/profile/#{current_user}"
+end
--- app/controllers/task.rb
+++ app/controllers/task.rb
@@ -21,3 +21,28 @@ get '/tasks/complete/:id' do
   @task.save
   erb :"/tasks/show"
 end
+
+get '/tasks/:id/edit' do
+  require_user
+  @task = Task.find(params[:id])
+  erb :'/tasks/edit'
+end
+
+put '/tasks/:id' do
+  require_user
+  @task = Task.find(params[:id])
+  @task.assign_attributes(params[:task])
+  if @task.save
+    redirect "/lists/#{@task.list_id}"
+  else
+    @errors = @task.errors.full_messages
+    erb :'/tasks/edit'
+  end
+end
+
+delete '/tasks/:id' do
+  require_user
+    temp = params[:task][:list_id]
+    Task.find(params[:id]).destroy
+    redirect "/lists/#{temp}"
+end
