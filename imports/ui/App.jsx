cb308c790b293b1d3571f17608de4c49ecd622eb
Chainsaw Bucktooth <email": null>
2016-08-28 14:14:12 -0400

    modified Task and App to store temporary UI data in component state

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -6,6 +6,17 @@ import { Tasks } from '../api/tasks.js';
 import Task from './Task.jsx';
 
 class App extends Component {
+    //React components have a special field called 'state' where you can store encapsulated component data
+    constructor(props) {
+      super(props);
+
+    //here we add an initial state to App component
+      this.state = {
+        hideCompleted: false,
+      };
+    }
+   //we can update this.state from an event handler by calling this.setState, which will then update the state property asynchronously and then cause the component to re-render
+
     handleSubmit(event) {
       event.preventDefault();
 
@@ -21,8 +32,19 @@ class App extends Component {
       ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
 
+    toggleHideCompleted() {
+      this.setState({
+        hideCompleted: !this.state.hideCompleted,
+      });
+    }
+
+
      renderTasks() {
-       return this.props.tasks.map((task) => (
+       let filteredTasks = this.props.tasks;
+       if (this.state.hideCompleted) {
+         filteredTasks = filteredTasks.filter(task => !task.checked);
+       }
+       return filteredTasks.map((task) => (
          <Task key={task._id} task={task} />
        ));
      }
@@ -31,7 +53,17 @@ class App extends Component {
        return (
          <div className="container">
            <header>
-             <h1>Todo List</h1>
+             <h1>Todo List ({this.props.incompleteCount})</h1>
+              <label className="hide-completed">
+                <input
+                  type="checkbox"
+                  readOnly
+                  checked={this.state.hideCompleted}
+                  onClick={this.toggleHideCompleted.bind(this)}
+                />
+                Hide Completed Tasks
+              </label>
+
 
               <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                 <input
@@ -52,10 +84,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
